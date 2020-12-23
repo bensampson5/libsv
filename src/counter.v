91f9743528070025ad859afada8ad06b20f0f18a
Ben Sampson <bensampson5@gmail.com>
2020-12-23 17:11:16 -0600

    Add async active low reset to counter

--- src/counter.v
+++ src/counter.v
@@ -2,11 +2,15 @@
 
 module counter #(parameter N = 8)
 (
-    input clk,
+    input  wire       clk,
+    input  wire       aresetn,
     output reg[N-1:0] q
 );
 
-    always @ (posedge clk)
-        q <= q + 1;
+    always @ (posedge clk or negedge aresetn)
+        if (!aresetn)
+            q <= 0;
+        else
+            q <= q + 1;
 
 endmodule
