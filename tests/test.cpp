291b71d17262068aa89c6b38fb900a0ef54e3466
Ben Sampson <bensampson5@gmail.com>
2020-12-29 12:01:59 -0600

    Write unit test for counter

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,2 +1,17 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
+#include "verilated.h"
+#include "verilated_vpi.h"
+
+// For verilator's VPI
+vluint64_t main_time = 0;
+double sc_time_stamp() { return main_time; }
+
+int get_module_parameter(const char* const handle)
+{
+    vpiHandle vh = vpi_handle_by_name((PLI_BYTE8*)handle, NULL);
+    s_vpi_value v;
+    v.format = vpiIntVal;
+    vpi_get_value(vh, &v);
+    return v.value.integer;
+}
