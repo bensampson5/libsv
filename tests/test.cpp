f7824950b2ad4bb64faf654fc916a00450f14915
Ben Sampson <bensampson5@gmail.com>
2020-12-31 09:15:16 -0600

    move test_counter in core folder within tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,6 +2,7 @@
 #include "catch2/catch.hpp"
 #include "verilated.h"
 #include "verilated_vpi.h"
+#include <cstdint>
 
 // For verilator's VPI
 vluint64_t main_time = 0;
@@ -15,3 +16,27 @@ int get_module_parameter(const char* const handle)
     vpi_get_value(vh, &v);
     return v.value.integer;
 }
+
+// template<class MODULE> class TESTBENCH {
+//     uint64_t m_tickcount;
+//     MODULE *m_module;
+
+//     TESTBENCH(void)
+//     {
+//         m_module = new MODULE;
+//         m_tickcount = 0;
+//     }
+
+//     virtual ~TESTBENCH(void)
+//     {
+//         delete m_module;
+//         m_module = nullptr;
+//     }
+
+//     virtual void reset(int dir)
+//     {
+//         m_module->
+//         this->tick();
+//     }
+
+// }
