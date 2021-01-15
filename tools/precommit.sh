b36dd18871e8f306d9a6227a297f5ede2b4aa47d
Ben Sampson <bensampson5@gmail.com>
2021-01-15 12:17:20 -0600

    Remove old precommit.sh

--- tools/precommit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Note: Should be run from project root directory
-
-# Format all SystemVerilog/Verilog files
-verible-verilog-format --inplace $(find . -type f  \( -name "*.sv" -o -name "*.v" \))
