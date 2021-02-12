ec7e4c3df1478b1139363555af13b3e589199c42
Ben Sampson <bensampson5@gmail.com>
2021-02-12 14:53:16 -0600

    Remove sphinx-hdlcontrib package and add conf.py directory to path for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,17 +5,16 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import sphinx_rtd_theme
-import sphinxcontrib_hdl_diagrams
 
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,8 +30,7 @@ author = 'Ben Sampson'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
-    "sphinxcontrib_hdl_diagrams"
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,3 +53,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Generate svg images from HDL source files
+from helper import gen_hdl_svg
+gen_hdl_svg()
