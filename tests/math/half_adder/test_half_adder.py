5abeb3dcaec99a5ebd3c720281d2e46473f90544
Ben Sampson <bensampson5@gmail.com>
2021-07-06 12:44:21 -0500

    Add a half_adder testbench in cocotb

---
+++ tests/math/half_adder/test_half_adder.py
@@ -0,0 +1,22 @@
+from cocotb_test.simulator import run
+import os
+from pathlib import Path
+
+def test_half_adder(pytestconfig):
+
+    os.environ["SIM"] = "verilator"
+    proj_path = Path(pytestconfig.rootpath)
+    build_dir = proj_path / "build" / "math" / "half_adder"
+
+    run(
+        verilog_sources=[proj_path / "src" / "math" / "half_adder" / "half_adder.sv"],
+        toplevel="half_adder",
+        module="half_adder_cocotb",
+        sim_build=build_dir,
+        waves=True,
+    )
+
+    wavefile = build_dir / "dump.fst"
+    if wavefile.exists():
+        wavefile.rename(build_dir / "test_half_adder.rst")
+
