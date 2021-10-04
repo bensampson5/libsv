5ec01edd7b180fa8700b553dd4fd4a56b96f0bd8
Ben Sampson <bensampson5@gmail.com>
2021-10-03 20:23:43 -0500

    Generic implementation

--- src/encode_decode/bcd_encoder/bcd_encoder.sv
+++ src/encode_decode/bcd_encoder/bcd_encoder.sv
@@ -1,29 +1,19 @@
-module bcd_encoder (
-    input  logic [3:0] bin,
-    output logic [7:0] bcd
+module bcd_encoder #(
+    parameter integer N  /* verilator public_flat_rd */ = 8
+) (
+    input  logic [      N-1:0] bin,
+    output logic [N+(N-4)/3:0] bcd
 );
 
-  always_comb begin
-    case (bin)
-      4'h0:    bcd = 8'h00;
-      4'h1:    bcd = 8'h01;
-      4'h2:    bcd = 8'h02;
-      4'h3:    bcd = 8'h03;
-      4'h4:    bcd = 8'h04;
-      4'h5:    bcd = 8'h05;
-      4'h6:    bcd = 8'h06;
-      4'h7:    bcd = 8'h07;
-      4'h8:    bcd = 8'h08;
-      4'h9:    bcd = 8'h09;
-      4'hA:    bcd = 8'h10;
-      4'hB:    bcd = 8'h11;
-      4'hC:    bcd = 8'h12;
-      4'hD:    bcd = 8'h13;
-      4'hE:    bcd = 8'h14;
-      4'hF:    bcd = 8'h15;
-      default: bcd = 8'h00;
-    endcase
-  end
+  integer i, j;
 
+  always_comb begin
+    for (i = 0; i <= N + (N - 4) / 3; i = i + 1) bcd[i] = 0;  // initialize with zeros
+    bcd[N-1:0] = bin;  // initialize with input vector
+    for (i = 0; i <= N - 4; i = i + 1)  // iterate on structure depth
+      for (j = 0; j <= i / 3; j = j + 1)  // iterate on structure width
+        if (bcd[N-i+4*j-:4] > 4)  // if > 4
+          bcd[N-i+4*j-:4] = bcd[N-i+4*j-:4] + 4'd3;  // add 3
+  end
 
 endmodule
--- tests/encode_decode/bcd_encoder/test_bcd_encoder.py
+++ tests/encode_decode/bcd_encoder/test_bcd_encoder.py
@@ -12,20 +12,9 @@ def test_bcd_encoder(pytestconfig):
 async def cocotb_test_bcd_encoder(dut):
     """BCD Encoder test"""
 
-    # dw = int(dut.DW)
-    # n = int(dut.N)
+    n = int(dut.N)
 
-    for i in range(16):
+    for i in range(2 ** n):
         dut.bin <= i
         await Timer(1)
-
-    # # Generate counting pattern on input vectors
-    # dut_i = 0
-    # for i in range(n):
-    #     dut_i |= (i % dw) << (i * dw)
-    # dut.i <= dut_i
-
-    # for i in range(n):
-    #     dut.sel <= 1 << i
-    #     await Timer(1)
-    #     assert i % dw == int(dut.o)
+        assert dut.bcd == int(str(int(dut.bin)), 16)
