c1df8f4af56f7189db87cfb23517761fd4d6b365
Ben Sampson <bensampson5@gmail.com>
2021-10-29 10:42:33 -0500

    Change copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath("."))
 # -- Project information -----------------------------------------------------
 
 project = "LibSV"
-copyright = "2021, Ben Sampson"
+copyright = "2020-2021, Ben Sampson"
 author = "Ben Sampson"
 
 
