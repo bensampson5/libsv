4bca82d71b999da5774b391634f01c3b94f7f8d3
Ben Sampson <bensampson5@gmail.com>
2022-01-27 21:01:45 -0600

    Add priority encoder

---
+++ libsv/coders/priority_encoder.sv
@@ -0,0 +1,24 @@
+module priority_encoder #(
+    parameter integer IW  /* verilator public_flat_rd */ = 4
+) (
+    input  logic [        IW-1:0] i_in,
+    output logic [$clog2(IW)-1:0] o_out,
+    output logic                  o_valid
+);
+
+  always_comb begin
+    bit stop;
+    o_out   = '0;
+    o_valid = 1'b0;
+    stop    = 1'b0;
+
+    for (int i = 0; i < IW; ++i) begin
+      if (i_in[i] == 1'b1 && !stop) begin
+        o_out   = i[$clog2(IW)-1:0];
+        o_valid = 1'b1;
+        stop    = 1'b1;
+      end
+    end
+  end
+
+endmodule
