e1c3350376ea0307cf4675d7f0789d65da564743
Ben Sampson <bensampson5@gmail.com>
2022-01-29 15:36:29 -0600

    Remove valid from onehot_priority_encoder because not needed

--- libsv/coders/onehot_priority_encoder.sv
+++ libsv/coders/onehot_priority_encoder.sv
@@ -2,23 +2,20 @@
 `define LIBSV_CODERS_ONEHOT_PRIORITY_ENCODER
 
 module onehot_priority_encoder #(
-    parameter integer IW  /* verilator public_flat_rd */ = 4
+    parameter integer WIDTH  /* verilator public_flat_rd */ = 4
 ) (
-    input  logic [IW-1:0] i_in,
-    output logic [IW-1:0] o_out,
-    output logic          o_valid
+    input  logic [WIDTH-1:0] i_in,
+    output logic [WIDTH-1:0] o_out
 );
 
   always_comb begin
     bit stop;
     o_out   = '0;
-    o_valid = 1'b0;
     stop    = 1'b0;
 
-    for (int i = 0; i < IW; ++i) begin
+    for (int i = 0; i < WIDTH; ++i) begin
       if (i_in[i] == 1'b1 && !stop) begin
         o_out[i] = 1'b1;
-        o_valid  = 1'b1;
         stop     = 1'b1;
       end
     end
