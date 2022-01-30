de03a89f741d20a38643fea176b8f0f903df68d1
Ben Sampson <bensampson5@gmail.com>
2022-01-29 18:05:48 -0600

    Add simple testbench for ring arbiter

--- tests/arbiters/test_ring_arbiter.py
+++ tests/arbiters/test_ring_arbiter.py
@@ -1,6 +1,7 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import FallingEdge
+from cocotb.log import SimLog
 from utils import pytest_cocotb_run_test
 
 
@@ -13,8 +14,10 @@ def test_ring_arbiter(pytestconfig):
 async def cocotb_test_ring_arbiter(dut):
     """Ring Arbiter test"""
 
-    # ports = int(dut.PORTS)
-    # data_width = int(dut.DATA_WIDTH)
+    log = SimLog("cocotb.test_ring_arbiter")
+
+    ports = int(dut.PORTS)
+    data_width = int(dut.DATA_WIDTH)
 
     cocotb.fork(Clock(dut.i_clock, 2).start())
 
@@ -23,9 +26,33 @@ async def cocotb_test_ring_arbiter(dut):
     await FallingEdge(dut.i_clock)
     dut.i_aresetn.value = 1
 
-    dut.i_data.value = 0x04030201
-    dut.i_input_valid.value = 0b1111
+    # setup i_data
+    i_data_array = [ports-i-1 for i in range(ports)]
+    i_data = 0
+    for i in range(ports):
+        i_data |= (ports-i-1) << (data_width*(ports-i-1))
+    log.info(f"i_data = 0x" + format(i_data, f"0{ports*data_width//4}x"))
+    log.info(f"i_data_array = {i_data_array}")
+    dut.i_data.value = i_data
+    dut.i_input_valid.value = 2**ports - 1
 
+    # Scenario 1: Have all ports have data ready for the arbiter and
+    # have output interface be ready to receive data from arbiter. This
+    # will test the ring arbiter at max throughput
     dut.i_output_ready.value = 1
-    for i in range(10):
+    for i in range(4*ports):
         await FallingEdge(dut.i_clock)
+        if i > 0:
+            assert (int(dut.o_data.value) + 1) % ports == i % ports
+
+    # Scenario 2: Have a port drop out but still maintain max throughput
+    # of output interface
+    # TODO
+
+    # Scenario 3: Have the output interface backpressure the ring arbiter
+    # which should cause it to fill up and backpressure the input interface
+    # TODO
+
+    # Scenario 4: Have all ports drop out but release the backpressing of the
+    # ring arbiter by the output interface and watch the ring arbiter go empty
+    # TODO
