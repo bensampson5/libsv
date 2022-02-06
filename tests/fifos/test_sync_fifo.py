104787109fcdc59e97ffa3e2e89f342dc07f66d5
Ben Sampson <bensampson5@gmail.com>
2022-02-06 12:12:32 -0600

    Sync fifo (#143)
    * Add sync_fifo + testbench
    
    * Change sync_fifo default FIFO_DEPTH to 4

---
+++ tests/fifos/test_sync_fifo.py
@@ -0,0 +1,78 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge
+from utils import pytest_cocotb_run_test
+
+
+def test_sync_fifo(pytestconfig):
+    """Pytest fixture for Sync Fifo test"""
+    pytest_cocotb_run_test(pytestconfig, __name__)
+
+
+@cocotb.test()
+async def cocotb_test_sync_fifo(dut):
+    """Sync Fifo test"""
+
+    data_width = int(dut.DATA_WIDTH)
+    fifo_depth = int(dut.FIFO_DEPTH)
+
+    cocotb.fork(Clock(dut.i_clock, 2).start())
+
+    # reset
+    dut.i_aresetn.value = 0
+    await FallingEdge(dut.i_clock)
+    dut.i_aresetn.value = 1
+
+    # fill up fifo completely
+    dut.i_wr_en.value = 1
+    for i in range(1, fifo_depth + 1):
+        dut.i_data.value = i % data_width
+        await FallingEdge(dut.i_clock)
+
+    assert int(dut.o_full.value) == 1
+    assert int(dut.o_empty.value) == 0
+
+    # read out everything from fifo completely
+    dut.i_wr_en.value = 0
+    dut.i_rd_en.value = 1
+    for i in range(1, fifo_depth + 1):
+        assert int(dut.o_data.value) == i % data_width
+        await FallingEdge(dut.i_clock)
+
+    assert int(dut.o_full.value) == 0
+    assert int(dut.o_empty.value) == 1
+
+    #  test streaming with 1 entry in fifo
+    for i in range(1, 6):
+        if i == 1:
+            dut.i_wr_en.value = 1
+            dut.i_data.value = i % data_width
+            await FallingEdge(dut.i_clock)
+            dut.i_rd_en.value = 1
+        else:
+            assert int(dut.o_data.value) == (i - 1) % data_width
+            dut.i_data.value = i % data_width
+            await FallingEdge(dut.i_clock)
+
+        assert int(dut.o_empty.value) == 0  # never is empty
+
+    # test streaming with fifo_depth-1 entries in fifo
+    dut.i_wr_en.value = 1
+    dut.i_rd_en.value = 0
+    dut.i_aresetn.value = 0
+    await FallingEdge(dut.i_clock)
+    dut.i_aresetn.value = 1
+    for i in range(1, fifo_depth + 5):
+        if i < fifo_depth:  # fill up fifo
+            dut.i_data.value = i % data_width
+            await FallingEdge(dut.i_clock)
+            if i == fifo_depth - 1:
+                dut.i_rd_en.value = 1
+        else:
+            assert int(dut.o_data.value) == (i - (fifo_depth - 1)) % data_width
+            dut.i_data.value = i % data_width
+            await FallingEdge(dut.i_clock)
+
+        assert int(dut.o_full.value) == 0  # never is full
+
+    await FallingEdge(dut.i_clock)
