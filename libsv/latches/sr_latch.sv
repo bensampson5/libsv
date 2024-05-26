be2ebd1d883ee91f59ef06887685c21f07f25775
Ben Sampson <bensampson5@gmail.com>
2024-05-25 17:32:25 -0700

    fix verilator waiver

--- libsv/latches/sr_latch.sv
+++ libsv/latches/sr_latch.sv
@@ -4,7 +4,7 @@
 module sr_latch (
     input  logic                                 s,
     input  logic                                 r,
-    output logic  /* verilator lint_off UNOPT */ q  /* verilator lint_on UNOPT */,
+    output logic  /* verilator lint_off UNOPTFLAT */ q  /* verilator lint_on UNOPTFLAT */,
     output logic                                 q_n
 );
 
